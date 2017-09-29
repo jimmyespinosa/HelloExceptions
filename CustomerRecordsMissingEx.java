22da3ab0d88cbdafe695e9246a77981cc9a7a695
Jimmy Espinosa <jimmyespinosa89@gmail.com>
2017-09-28 21:41:36 -0400

    new class added

---
+++ CustomerRecordsMissingEx.java
@@ -0,0 +1,9 @@
+
+public class CustomerRecordsMissingEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- HelloExceptions.java
+++ HelloExceptions.java
@@ -3,6 +3,18 @@ public class HelloExceptions {
 
 	public static void main(String[] args) {
 		int a = 10, b = 6, c;
+		
+		
+
+		if (a > b){
+			throw new ArithmeticException("We have a problem.");
+		}
+		
+		//ArithmeticException ae = new ArithmeticException("We have a problem.");
+		/*
+		 * Throw this exception and the rest of the code will not run!
+		 */
+		//throw ae;
 
 		// a = b / 0;
 		System.out.println("in main");
@@ -20,12 +32,14 @@ public class HelloExceptions {
 			System.out.println("Dealing with a problem!");
 		} catch (Exception e){
 			System.out.println("Another problem???");
+		} finally {
+			System.out.println("I will always happen!!!!!!");
 		}
 		
 		System.out.println("didn't get here");
 	}
 
-	private static void foo2() {
+	private static void foo2() throws ArithmeticException{
 		int e = 45;
 		System.out.println("in foo2");
 		
