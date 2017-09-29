7ef538680b36b65995fc9903786a114573c3fe02
Jimmy Espinosa <jimmyespinosa89@gmail.com>
2017-09-28 20:21:35 -0400

    playing with exceptions

--- HelloExceptions.java
+++ HelloExceptions.java
@@ -2,8 +2,36 @@
 public class HelloExceptions {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-  
+		int a = 10, b = 6, c;
+
+		// a = b / 0;
+		System.out.println("in main");
+		foo();
+		System.out.println("didn't get here");
+
+	}
+
+	private static void foo() {
+		System.out.println("In foo");
+		
+		try{
+			foo2();
+		} catch (ArithmeticException ae){
+			System.out.println("Dealing with a problem!");
+		} catch (Exception e){
+			System.out.println("Another problem???");
+		}
+		
+		System.out.println("didn't get here");
+	}
+
+	private static void foo2() {
+		int e = 45;
+		System.out.println("in foo2");
+		
+			e = 1 / 0;
+		
+		System.out.println("didn't get here");
 	}
 
 }
